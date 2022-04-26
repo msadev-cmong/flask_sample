877f7f9439274128e2558e978f402cd35917f5af
Sungsil Chang <siluniv@naver.com>
2022-04-26 11:10:49 +0900

    added extend files

--- dev.py
+++ dev.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import pymysql
 
 app = Flask(__name__)
@@ -13,10 +13,19 @@ def home():
         labels.append(item[1])
         value.append(item[2])
 
-    return render_template('test.html', headquaters=labels, data=value)
+    return render_template('home.html', headquaters=labels, data=value)
+
+@app.route('/sido/<int:sido_id>')
+def show_detail(sido_id):
+
+    data = get_sub_data(sido_id)
+
+    print(data)
+
+    return render_template('detail.html', data=data)
 
 def get_main_data():
-    conn = pymysql.connect(host='mysql.sil', port=3306, user='test', password='Test!234', db='test_db', charset='utf8')
+    conn = pymysql.connect(host='localhost', port=3306, user='test', password='test', db='test_db', charset='utf8')
     cursor = conn.cursor()
     sql_main =  'SELECT * FROM main'
     cursor.execute(sql_main)
@@ -25,6 +34,17 @@ def get_main_data():
     conn.commit()
     conn.close()
     return data
+
+def get_sub_data(sido_id):
+    conn = pymysql.connect(host='localhost', port=3306, user='test', password='test', db='test_db', charset='utf8')
+    cursor = conn.cursor()
+    sql_main =  f'SELECT * FROM sub_table WHERE id = {sido_id + 1}'
+    cursor.execute(sql_main)
+    data = cursor
+
+    conn.commit()
+    conn.close()
+    return data.fetchone()
     
 if __name__ == "__main__":
-     app.run(host="0.0.0.0")
+     app.run(host="0.0.0.0", debug=True)
