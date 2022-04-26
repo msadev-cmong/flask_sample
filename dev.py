94d04cc46ae6d9811736b88e3adf5838f480cb87
Sungsil Chang <siluniv@naver.com>
2022-04-26 12:44:06 +0900

    fix templates

--- dev.py
+++ dev.py
@@ -15,14 +15,21 @@ def home():
 
     return render_template('home.html', headquaters=labels, data=value)
 
-@app.route('/sido/<int:sido_id>')
-def show_detail(sido_id):
+@app.route('/sido/<sido>')
+def show_detail(sido):
 
-    data = get_sub_data(sido_id)
+    if sido == '서울':
+        sido = '소방'
 
-    print(data)
+    data = get_sub_data(sido)
+    if data == None:
+        list_data = [0, '경기']
+    else:
+        list_data = list(data)
+        if list_data[1] == '소방':
+            list_data[1] = '서울'
 
-    return render_template('detail.html', data=data)
+    return render_template('detail.html', data=list_data)
 
 def get_main_data():
     conn = pymysql.connect(host='localhost', port=3306, user='test', password='test', db='test_db', charset='utf8')
@@ -38,7 +45,7 @@ def get_main_data():
 def get_sub_data(sido_id):
     conn = pymysql.connect(host='localhost', port=3306, user='test', password='test', db='test_db', charset='utf8')
     cursor = conn.cursor()
-    sql_main =  f'SELECT * FROM sub_table WHERE id = {sido_id + 1}'
+    sql_main =  f'SELECT * FROM sub_table WHERE 시도 = "{sido_id}"'
     cursor.execute(sql_main)
     data = cursor
 
